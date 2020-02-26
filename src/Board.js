a3f29c97e794f93b5baa6f59e8df8eda69488043
adrienn timko <adrienn.timko@gmail.com>
2020-02-26 10:03:52 +0100

    check winner, new game portal

--- src/Board.js
+++ src/Board.js
@@ -1,53 +1,84 @@
 import React, { useReducer } from 'react';
+import ReactDOM from 'react-dom';
 import Grid from './Grid';
+import checkWinner from './CheckWinner';
 
-const BoardCircles = new Array(7 * 6).fill('white'); //creates the board
 //console.log(BoardCircles);
+let gameEnds;
 
-export default function Board(){
-    const [state, dispatch] = useReducer(reducer, { color: 'teal', circles: BoardCircles });
+function initState() {
+    const BoardCircles = new Array(7 * 6).fill('white'); //creates the board
+    return { color: 'teal', circles: BoardCircles, winner: null, newGame: false };
+}
 
-    function reducer(state, action) {
-        switch (action.type) {
-            case 'drop_disc':
-                const newCircles = [...state.circles];
-                const column = action.index % 7; //creates columns
-                let index = 5 * 7 + column; //"creates" the indexes
-                
-                while (index >= 0) {
-                    if (newCircles[index] === 'white') {
-                        break; 
-                    }
-                    index -= 7;
-                }
-                
-                if (index < 0) { //when a column is filled 
-                    return state; //it should not be possible to drop into that column again
-                }
-
-                console.log(index);
-                newCircles[index] = state.color;
-                
-                return {
-                    ...state,
-                    color: state.color === 'teal' ? 'gold' : 'teal',
-                    circles: newCircles,
-                }
-            default:
+function reducer(state, action) {
+    switch (action.type) {
+        case 'drop_disc':
+            if (state.winner) {
                 return state;
-        }
+            }
+
+            const newCircles = [...state.circles];
+            const column = action.index % 7; //creates columns
+            let index = 5 * 7 + column; //"creates" the indexes
+
+            while (index >= 0) {
+                if (newCircles[index] === 'white') {
+                    break;
+                }
+                index -= 7;
+            }
+
+            if (index < 0) { //when a column is filled 
+                return state; //it should not be possible to drop into that column again
+            }
+
+            //console.log(index);
+            newCircles[index] = state.color;
+
+            const winner = checkWinner(newCircles);
+
+            console.log("WINNER IS", winner);
+
+            return {
+                ...state,
+                winner: winner,
+                color: state.color === 'teal' ? 'gold' : 'teal',
+                circles: newCircles,
+            }
+        case 'new_game':
+            return initState();
+        default:
+            return state;
+
+
+    }
+}
+
+
+export default function Board() {
+    const [state, dispatch] = useReducer(reducer, null, initState);
+
+    if (state.winner) {
+        console.log(state.winner)
+        gameEnds = (
+            <div className="game-ends" style={{ position: "absolute" }}>
+                <p>GAME OVER</p>
+                <h3>The winner is {state.color === 'teal' ? 'gold' : 'teal'}!</h3>
+                <button onClick={() => dispatch({ type: "new_game"})}>New Game</button>
+            </div>
+        )
     }
 
-    //if click on: index || index + 7 || index + 14 || index + 21 ... 
-    //=> drop in that column on the bottom
-
-    return(
+    return (
         <div>
             <h3>ConnectFour</h3>
-            <Grid 
+            <Grid
                 circles={state.circles}
-                onClick={(i) => dispatch({type: 'drop_disc', index: i})}
+                onClick={(i) => dispatch({ type: 'drop_disc', index: i })}
             />
+            {gameEnds}
+            {ReactDOM.createPortal(gameEnds, document.body)}
         </div>
     )
 }
---
+++ src/CheckWinner.js
@@ -0,0 +1,68 @@
+const checkWinner = (board) => {
+    /*--------------------------------- in rows -----------------------------*/
+    for (let x = 0; x <= 35; x += 7) {
+        for (let y = x; y < x + 3; y++) {
+            if (board[y] !== 'white') {
+                if (
+                    board[y] === board[y + 1] &&
+                    board[y] === board[y + 2] &&
+                    board[y] === board[y + 3]
+                ) {
+                    return board[y];
+                }
+            }
+        }
+    }
+    
+    /*--------------------------------- in columns -----------------------------*/
+    for (let x = 0; x <= 6; x++) {
+        for (let y = x; y <= x + 14; y += 7) {
+            if (board[y] !== 'white') {
+                if (
+                    board[y] === board[y + 7] &&
+                    board[y] === board[y + 14] &&
+                    board[y] === board[y + 21]
+                ) {
+                    return board[y];
+                }
+            }
+        }
+    }
+    /*--------------------- diagonals from the top left corner -----------------------*/
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 4; y++) {
+            let index = x * 7 + y;
+            if (board[index] !== 'white') {
+                if (
+                    board[index] === board[index + 8] &&
+                    board[index] === board[index + 16] &&
+                    board[index] === board[index + 24]
+                ) {
+                    console.log('How won?');
+                    return board[index];
+                }
+            }
+        }
+    }
+    
+    /*------------------------ diagonals from the top right corner -----------------------*/
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 4; y++) {
+            let index = x * 7 + y + 21;
+            if (board[index] !== 'white') {
+                if (
+                    board[index] === board[index - 6] &&
+                    board[index] === board[index - 12] &&
+                    board[index] === board[index - 18]
+                ) {
+                    console.log('How won?');
+                    return board[index];
+                }
+            }
+        }
+    }
+
+    return null;
+}
+
+export default checkWinner;
